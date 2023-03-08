b62f5ddc8e5cfbd6a37b969f06e075b58e4c611b
hyerinrhee <toohappy2122@gmail.com>
2023-03-08 18:03:04 +0900

    feat(TDD:문자열 덧셈 계산기): TDD 파트의 덧셈 문자열 계산기

--- src/test/java/study/caculator/CalculatorTest.java
+++ src/test/java/study/caculator/CalculatorTest.java
@@ -1,19 +1,17 @@
-package study;
+package study.caculator;
 
 import caculator.Caculator;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class CaculatorTest {
+public class CalculatorTest {
     @DisplayName("문자열 계산기 정상 작동을 확인해보자")
     @ParameterizedTest
     @CsvSource(value = {"2 + 3 * 4 / 2 | 10.0"}, delimiter = '|')
-    void caculator(final String string, final float result) {
+    void calculator(final String string, final float result) {
         Caculator calculator = new Caculator();
         System.out.println(calculator.caculator(string));
         assertThat(calculator.caculator(string)).isEqualTo(result);
